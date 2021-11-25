8d6e2e402b4cce14b387cf098fb03b1ba64cc042
jayson7 <lexxiijoo70@gmail.com>
2021-11-25 01:39:08 +0100

    wk2

---
+++ wk2/1.py
@@ -0,0 +1,49 @@
+# import re 
+# #the house is for python
+# txt = 'That big house is for python'
+# p = re.search("^That.*python$", txt)
+# #?
+# #^
+# #\
+# #$
+# #+
+# #{}
+# if p:
+#     print('yes')
+# else:
+#     print('no')
+# def o (s):
+#     print(s + 10)
+#     # return s
+# o(9)
+# def long_function (s, f):
+#     print(s / f)
+#     print("we are doing python")
+#     print("python")
+# long_function(18, 3)
+# # python lambda 
+# # o = lambda s :   s + 10  
+# # print(o(9))  
+# o = lambda s, f :   s / f  
+# print(o(9, 3))  
+
+
+# **kwargs
+#arbitrary keyword arguments
+
+# def pythons(*department):
+#     print('this is department' ,  department )
+
+
+# pythons('ML', 'Ai' ,'data science' , 'software devolopment' )
+
+
+# kwargs
+
+# def pythons(**department):
+#     print('this is department' ,  department )
+
+
+# pythons(f = 1, web_dev = 0.888, g = 'ml', h = 'ai' )
+
+
