bde918c413734ff5f0a68eb4d899167070150536
jayson7 <lexxiijoo70@gmail.com>
2021-11-26 21:25:36 +0100

    wk2

--- Date module/date.py
+++ Date module/date.py
@@ -5,27 +5,39 @@ the datetime module isused to gather up things that has to do with date and time
 # datetime 
 import datetime 
 from datetime import  time , date 
+
 from datetime import datetime as  d  
-# full datetime output
-print(d.now())
+
+#full datetime output
+# print(d.now())
 
 # print(dir(d.today))
-print(d.today())
+# print(d.today())
 
-# year 
+# # year 
+# name = 'myriam'
+# print(f" the prof in the class is {name} ")
+# print(datetime.datetime.now().year)
+# print(datetime.datetime.now().strftime("%y"))
+#%a
+#%A
+#%w
+#%d
+#%b
+#%B
+#%m
+#y
+# print(datetime.datetime.now().strftime("%B") )
+#%p AM/PM
 
-print(datetime.datetime.now().year)
-print(datetime.datetime.now().strftime("%A"))
-print(datetime.datetime.now().strftime("%B") )
 
+# #  only date 
+# print(date.today())
 
-
-#  only date 
-print(date.today())
-
-#  only time  using strif time
+# #  only time  using strif time
 strTime = datetime.datetime.now().strftime("%H:%M:%S")   
 print(strTime)
 
-strDate = datetime.datetime.now().strftime("%d:%m:%y")   
-print(strDate)
+
+# strDate = datetime.datetime.now().strftime("%d:%m:%y")   
+# print(strDate)
