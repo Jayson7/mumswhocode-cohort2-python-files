78704dd14ec5b9be6b9ee27d6a61d82fcf5b9fe4
jayson7 <lexxiijoo70@gmail.com>
2022-01-19 19:47:55 +0100

    djngo class

--- django landing page/landingPageProject/landingApp/views.py
+++ django landing page/landingPageProject/landingApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+from django.shortcuts import HttpResponse
+
 # Create your views here.
+
+# class based views 
+# function based views
+
+
+def homepage(request):
+    
+    return render(request, 'index.html')
+
